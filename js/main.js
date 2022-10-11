46a90378d616f2de34f95d4a70d1da37f5337c96
Igor Filimonov <filimonov163@gmail.com>
2022-10-11 12:07:47 +0400

    Нужно больше функций v2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 // Функция, возвращающая случайное целое число из переданного диапазона [min, max] включительно.
 
-function getRndInteger(min, max) {
+const rndInteger = (min, max) => {
   if (min < 0 || max < 0) {
     return NaN;
   }
+
   const lower = Math.ceil(Math.min(min, max));
   const upper = Math.floor(Math.max(min, max));
   const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
 
-getRndInteger(1, 20);
+  return Math.floor(result);
+
+};
+
+rndInteger(1, 20);
 
 // Функция для проверки максимальной длины строки
 
-function validateStrLength (string, length) {
-  return string.length <= length;
-}
+const strLengthValidation = (string, length) => string.length <= length;
 
-validateStrLength('four', 4);
+strLengthValidation('four', 4);
